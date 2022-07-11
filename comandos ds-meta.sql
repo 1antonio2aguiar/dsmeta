5fc92c50d7552f979bf949dab8b3c8b751952528
Antonio Aguiar <antonio.oliveira@codiub.com.br>
2022-07-11 10:54:56 -0300

    Project created

---
+++ comandos ds-meta.sql
@@ -0,0 +1,16 @@
+github.com/devsuperior/sds-dsmeta
+
+
+instalar yarn
+-------------
+npm install --location=global yarn ou npm install --global yarn
+
+criando projeto react frontend
+==============================
+yarn create vite frontend --template react-ts
+
+depos de criado o projeto é necessario baixar as dependecias para isso vai na pasta frinend (terminal git bash) e executa o comando yarn
+
+para iniciar o servidor - yarn dev
+ http://localhost:3000
+ 
