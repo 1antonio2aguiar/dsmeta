558ef13b16cae8c175799601f96b77eba0bcc529
Antonio Aguiar <antonio.oliveira@codiub.com.br>
2022-07-11 12:07:33 -0300

    First componet

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,15 @@
+import NotificationButton from "./components/notificationButton"
 
 function App() {
-  return (<h1>Ola Tonhas</h1>)
+  return (
+    <>
+    
+    <h1>Ola Tonhas</h1>
+    <NotificationButton/>
+    
+    </>
+  
+  )
 }
 
 export default App
