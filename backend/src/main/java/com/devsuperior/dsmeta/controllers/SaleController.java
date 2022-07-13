8e93f052b557e47a0fa67e6a1b0d48d84ae673a5
Antonio Aguiar <antonio.oliveira@codiub.com.br>
2022-07-13 11:48:02 -0300

    api teste

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmeta.entities.Sale;
@@ -17,7 +18,9 @@ public class SaleController {
 	@Autowired private SaleService saleService;
 	
 	@GetMapping
-	public List<Sale> findSales() {
-		return saleService.findSales();
+	public List<Sale> findSales(
+			@RequestParam(value="minDate", defaultValue = "") String minDate, 
+			@RequestParam(value="maxDate", defaultValue = "") String maxDate) {
+		return saleService.findSales(minDate, maxDate);
 	}
 }
--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,5 +1,8 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +16,15 @@ public class SaleService {
 	
 	@Autowired private SaleRepository saleRepository;
 	
-	public List<Sale> findSales() {
-		return saleRepository.findAll();
+	public List<Sale> findSales(String minDate, String maxDate) {
+		
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
+		//return saleRepository.findAll(min, max);
+		return saleRepository.findSales(min, max); 
 	}
-
+	
 }
